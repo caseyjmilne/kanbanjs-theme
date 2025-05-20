a99778ce09c3baa6d2a6c4aab3c7ba781c8dd424
Casey Milne <cjmofficio@gmail.com>
2025-05-19 17:16:34 -0700

    Draft dropzone spacer.

--- kanban.class.js
+++ kanban.class.js
@@ -4,7 +4,6 @@ class KanBan {
     statuses      = null;
     statusCols    = [] // Used to track the dropzone elements created from status list.
     dragTicket    = null;
-    ticketShifted = null;
 
     // Requires tickets, statuses passed.
     constructor( tickets, statuses ) {
@@ -32,8 +31,8 @@ class KanBan {
             el.id = 'dropzone-' + status.key;
             el.classList.add('status-col');
             el.classList.add('status-col-' + status.key);
-            el.addEventListener("dragover", this.dragoverHandler)
-            el.addEventListener("drop", this.dropHandler)
+            el.addEventListener("dragover", this.dragoverHandler.bind(this));
+            el.addEventListener("drop", this.dropHandler.bind(this));
             dzc.appendChild(el);
 
             // Track the dropzone element to enable ticket insertion.
@@ -80,28 +79,45 @@ class KanBan {
         // Get the index of the ticket hovered over. 
         const ch = e.target.parentElement.children;
         const chArr = Array.from(ch);
-
-        console.log(chArr)
         const r = chArr.indexOf( e.target )
-        console.log(r)
 
-        // Verify that the target is a dropzone (and not a ticket).
+        // Verify that the target is the status column.
         const val = e.target.classList[0] === 'status-col';
         if(val) {
             // Enable dropping.
             e.preventDefault();
         }
 
-        // Verify that the target is a dropzone (and not a ticket).
+        // Verify that the target is a ticket.
         const isTicket = e.target.classList[0] === 'ticket';
         if(isTicket) {
 
             // Enable dropping.
             e.preventDefault();
 
-            // Shift ticket.
-            e.target.classList.add('ticket-shifted');
-            this.ticketShifted = e.target.id;
+            // Remove existing spacer dropzone.
+            const dropSpacerEls = document.querySelectorAll('.drop-spacer');
+            dropSpacerEls.forEach( ( dropSpacerEl, i ) => {
+                dropSpacerEl.remove();
+            });
+
+            // Add spacer dropzone before or after ticket.
+
+            const dropSpacerEl = document.createElement('div');
+            dropSpacerEl.classList.add('drop-spacer');
+
+            const dropY = e.clientY; // Cursor position while dropping.
+            console.log(this)
+            const isDropAbove = this.isDropAbove(dropY, e.target);
+
+            if (isDropAbove) {
+                // Insert before
+                e.target.parentElement.insertBefore(dropSpacerEl, e.target);
+            } else {
+                // Insert after
+                e.target.parentElement.insertBefore(dropSpacerEl, e.target.nextSibling);
+            }
+            
         }
 
     }
@@ -112,6 +128,14 @@ class KanBan {
         const ticketId = e.dataTransfer.getData("text");
         const ticketEl = document.getElementById( ticketId ); 
 
+        // Handle drop on drop spacer.
+        const isDropspacer = e.target.classList[0] === 'drop-spacer';
+        if(isDropspacer) {
+
+            console.log('SPACER DROP')
+
+        }
+
         // Handle drop on ticket.
         const isTicket = e.target.classList[0] === 'ticket';
         if(isTicket) {
@@ -119,22 +143,21 @@ class KanBan {
             console.log('IS TICKET, APPENDING')
 
             const dropY = e.clientY; // Cursor position while dropping.
-            const targetRect = e.target.getBoundingClientRect(); // Target ticket boundaries. 
-            const targetMiddleY = targetRect.top + targetRect.height / 2;
+            const isDropAbove = this.isDropAbove(dropY, e.target)
 
-            if (dropY < targetMiddleY) {
+            if (isDropAbove) {
                 // Insert before
                 e.target.parentElement.insertBefore(ticketEl, e.target);
-              } else {
+            } else {
                 // Insert after
                 e.target.parentElement.insertBefore(ticketEl, e.target.nextSibling);
-              }
+            }
             
             return;
 
         }
 
-         
+        // Default behavior when the drop target is the status column ticket is appended to the end.
         e.currentTarget.appendChild( ticketEl );
 
     }
@@ -144,11 +167,17 @@ class KanBan {
         const ticketEl = document.getElementById(this.dragTicket);
         ticketEl.classList.remove('ticket-dragged');
 
-        // Remove .ticket-shifted from tickets.
-        const shiftedTicketEls = document.querySelectorAll('.ticket-shifted');
-        shiftedTicketEls.forEach( ( ticketEl, i ) => {
-            ticketEl.classList.remove('ticket-shifted')
-        })
+    }
+
+    // Determine if the drop is on the upper half of the target ticket.
+    isDropAbove(dropY, target) {
+
+        const targetRect = target.getBoundingClientRect();
+        const targetMiddleY = targetRect.top + targetRect.height / 2;
+        if (dropY < targetMiddleY) {
+            return true;
+        }
+        return false;
 
     }
 
@@ -159,9 +188,6 @@ class KanBan {
 
 /****** @TODO 
 
-1. Shift ticket when dragover it to enable tickets to be placed in between tickets.
-    = During dragover event.
-2. Append ticket into list at the correct position when dropped. 
-    = During drop event. Requires identifying the index of the nearest ticket(s).
+1. Add spacer before or after ticket during dragover ticket.
 
 ********/
